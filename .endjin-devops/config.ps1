76a4418f1d188d9d137faef30992a8702f01be1c
James Dawson <james.dawson@endjin.com>
2024-09-10 23:46:24 +0100

    Update build config
    Treats the existing scripted build module as an extension

--- .endjin-devops/config.ps1
+++ .endjin-devops/config.ps1
@@ -7,18 +7,22 @@
 
 # Simple syntax where version constraints and/or custom process definition is not required
 # $devopsExtensions = @(
-#     "D:/Endjin.RP.Build/module"
+#     "Endjin.RecommendedPractices.Build"
+#     "../../Endjin.RecommendedPractices.Build/module"
 # )
 
 # Full syntax
 $devopsExtensions = @(
     @{
+        # Use latest stable version of exisiting scripted build module
         Name = "Endjin.RecommendedPractices.Build"
-        Path = "D:/Endjin.RP.Build/module"  # If Path is not specified, the module will be installed from the repository
-        # Repository = ""  # Allows the source repository to be overridden on a per extension basis
         Process = "tasks/build.process.ps1"
-        # TODO: Add support for obtaining extensions via Git
+        # Version = "1.5.10-beta0003"
+        # PreRelease = $true
+        # Path = "D:/Endjin.RP.Build/module"  # If Path is not specified, the module will be installed from the repository
+        # Repository = ""  # Allows the source repository to be overridden on a per extension basis
     }
+    # TODO: Add support for obtaining extensions via Git?
 )
 
 # Load the tasks and process
@@ -37,22 +41,25 @@ $PowerShellModulesToPublish = @(
 # Customise the build process
 task . FullBuild
 
-# build extensibility tasks
-task RunFirst {}
-task PreInit {}
-task PostInit {}
-task PreVersion {}
-task PostVersion {}
-task PreBuild {}
-task PostBuild {}
-task PreTest {}
-task PostTest {}
-task PreTestReport {}
-task PostTestReport {}
-task PreAnalysis {}
-task PostAnalysis {}
-task PrePackage {}
-task PostPackage {}
-task PrePublish {}
-task PostPublish {}
-task RunLast {}
+#
+# Build Process Extensibility Points - uncomment and implement as required
+#
+
+# task RunFirst {}
+# task PreInit {}
+# task PostInit {}
+# task PreVersion {}
+# task PostVersion {}
+# task PreBuild {}
+# task PostBuild {}
+# task PreTest {}
+# task PostTest {}
+# task PreTestReport {}
+# task PostTestReport {}
+# task PreAnalysis {}
+# task PostAnalysis {}
+# task PrePackage {}
+# task PostPackage {}
+# task PrePublish {}
+# task PostPublish {}
+# task RunLast {}
